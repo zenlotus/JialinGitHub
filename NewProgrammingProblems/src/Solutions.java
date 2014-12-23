e2086636fe8fabda2a3f6d52c1a845d4dc3b5281
zenlotus <jiaojialin@gmail.com>
2014-12-22 22:43:05 -0800

    initial checkin

---
+++ NewProgrammingProblems/src/Solutions.java
@@ -0,0 +1,14 @@
+
+/* 
+ * Served as my saved code. 
+ * 
+ */
+
+public class Solutions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1+3);
+	}
+
+}
