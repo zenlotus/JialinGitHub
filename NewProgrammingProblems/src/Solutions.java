b51c360613f54c62248d9a8fc2f3c7b4d662b680
zenlotus <jiaojialin@gmail.com>
2014-12-25 06:29:14 -0800

    add poj Post Office problem

--- NewProgrammingProblems/src/Solutions.java
+++ NewProgrammingProblems/src/Solutions.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 
 /* 
  * Served as my saved code. 
@@ -5,10 +7,28 @@
  */
 
 public class Solutions {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(3725/800);
-	}
+	/*
+	 * Leetcode problem: Excel Sheet Column Title
+	 * 
+	 * @author Jialin  
+	 */
+	
+	
+	
+    public String convertToTitle(int n) {
+        StringBuilder res = new StringBuilder();
+        if(n<=0) return res.toString();
+        while(n>0){
+            res.append((char)('A'+ ((n-1)%26)));
+            n = (n-1)/26;
+        }
+        return res.reverse().toString();
+    }
+    /*
+     * POJ 1160   - http://poj.org/problem?id=1160
+     */
+    public void PostOffice(){
+    	Scanner in = new Scanner(System.in);
+    }
 
 }
